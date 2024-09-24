320da01e89c30c3d36f207fbb3d263574927f010
Muhammad Hanif Abdillah <muhammadhanifa23@gmail.com>
2024-09-24 16:39:11 +0700

    style: clear comment

--- app/Activity.php
+++ app/Activity.php
@@ -30,13 +30,11 @@ class Activity
         return 3;
     }
 
-    // Perbaikan penggunaan ArrayIterator tanpa namespace
     public function getDataProvider(): iterable
     {
         return new \ArrayIterator(['user1', 'user2', 'user3']);
     }
 
-    // Perbaikan penggunaan stdClass tanpa namespace
     public function getDetailsObject(): object
     {
         return new \stdClass();
